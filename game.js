155b6327c9bb08a5ae93bf8866251b32daee9920
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-02 18:39:07 -0600

    Count total wins for each player and track the rounds
    Add the winning player in an array of all winning players. Read out the total numer of wins for each player at the end of each round.
    
    At the start of each round, delcare which round it is.

--- game.js
+++ game.js
@@ -1,61 +1,101 @@
+// Define winners
 
-// Computer selection
+let roundWinners = [];
 
-const computerOptions = ["rock", "paper", "scissors"]; // set options for computer selection
-        console.log(computerOptions);
+// Play 5 rounds, and stop after the 5th round
 
-const computerSelection = getComputerChoice(computerOptions); // set computer selection
-    console.log("The computer chose " + computerSelection);
+for (let i = 1; i < 6; i++) {
+    console.log(`It's time for round ${i}`);
 
-function getComputerChoice(computerOptions) { // computer randomly picks rock, paper, or scissors
+// For each round...
+
+// The computer selects an option: rock, paper or scissors
+
+    let computerOptions = ["rock", "paper", "scissors"]; // set options for computer selection
+
+    let computerSelection = getComputerChoice(computerOptions); // set computer selection
+        // console.log("The computer chose " + computerSelection);
+
+    function getComputerChoice(computerOptions) { // computer randomly picks rock, paper, or scissors
     let index = Math.floor(Math.random() * computerOptions.length); // select a random index (option) in the array.
     return computerOptions[index];
 }
 
-// Player selection
+// The player selects an option via prompt: rock, paper or scissors
 
-const playerInput = prompt("What do you choose?", "Type rock, paper, or scissors."); // player inputs choice
-const playerSelection = playerInput.toLowerCase(); // player choice is made all lower case
-    console.log("The player chose " + playerSelection);
+    let playerInput = prompt("What do you choose?", "Type rock, paper, or scissors."); // player inputs choice
+    let playerSelection = playerInput.toLowerCase(); // player choice is made all lower case
+        // console.log("The player chose " + playerSelection);
 
-// Define winner for tracking
+// The winner is determined of the round
 
-let winner;
-let roundResult;
+    function playRound(computerSelection, playerSelection) {
+        if ((computerSelection === "rock" && playerSelection === "scissors") ||
+            (computerSelection === "paper" && playerSelection === "rock") ||
+            (computerSelection === "scissors" && playerSelection === "paper")) {
+                console.log(`The computer won! ${computerSelection} beats ${playerSelection}.`);
+                return "computer";
+            }  // three possibilities where computer wins
+        else if ((computerSelection === "scissors" && playerSelection === "rock") ||
+            (computerSelection === "rock" && playerSelection === "paper") ||
+            (computerSelection === "paper" && playerSelection === "scissors")) {
+                console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
+                return "player";
+            }  // three possibilities where computer wins  
+        else {
+            console.log(`It was a tie! Both you and the computer chose ${computerSelection}.`);
+            return "tie";
+        }
+        
+    }
 
-// Play a round
+// Define the winner
 
-function playRound(computerSelection, playerSelection) {
-    if ((computerSelection === "rock" && playerSelection === "scissors") ||
-        (computerSelection === "paper" && playerSelection === "rock") ||
-        (computerSelection === "scissors" && playerSelection === "paper")) {
-            console.log("Computer won");
-            return winner = "computer";
-        }  // three possibilities where computer wins
-    else if ((computerSelection === "scissors" && playerSelection === "rock") ||
-        (computerSelection === "rock" && playerSelection === "paper") ||
-        (computerSelection === "paper" && playerSelection === "scissors")) {
-            console.log("Player won");
-            return winner = "player";
-        }  // three possibilities where computer wins  
-    else {
-        console.log("Tie");
-        return winner = "tie";
+    let winner = playRound(computerSelection, playerSelection);
+
+// Add the winner to the array
+
+roundWinners.push(winner);
+
+// Count the number of wins for the computer
+
+let computerWins = 0;
+for (let i = 0; i < roundWinners.length; i++) {
+    if (roundWinners[i] === "computer") {
+        ++computerWins
     }
 }
 
-// Declare the winner
+console.log(`The computer has won ${computerWins} times.`);
 
-function declareWinner(winner) {
-    if (winner === "computer") {
-        return roundResult = `You lose, since ${computerSelection} beats ${playerSelection}!`; 
-    } else if (winner === "player") {
-        return roundResult = `You win, since ${playerSelection} beats ${computerSelection}!`; 
-    } else return roundResult = `It's a tie, you both chose ${computerSelection}!`;
+// Count the number of wins for the player
+
+let playerWins = 0;
+for (let i = 0; i < roundWinners.length; i++) {
+    if (roundWinners[i] === "player") {
+        ++playerWins
+    }
 }
 
-console.log(playRound(computerSelection, playerSelection));
-console.log(winner);
-console.log(declareWinner(winner));
+console.log(`You have won ${playerWins} times.`);
 
-alert(declareWinner(winner));
+// Count the number of ties
+
+let ties = 0;
+for (let i = 0; i < roundWinners.length; i++) {
+    if (roundWinners[i] === "tie") {
+        ++ties
+    }
+}
+
+console.log(`There have been ${ties} ties.`);
+    
+}
+
+
+
+// To do:
+
+// Update the capitalization for the winning text.
+
+// Add an error so if they don't type in an acceptable selection, it asks them to retype.
--- randomcode.js
+++ randomcode.js
@@ -6,17 +6,19 @@ function playRound(computerSelection, playerSelection) {
     if ((computerSelection === "rock" && playerSelection === "scissors") ||
         (computerSelection === "paper" && playerSelection === "rock") ||
         (computerSelection === "scissors" && playerSelection === "paper")) {
-            console.log("working");
-            return winner = "computer"; 
+            console.log("Computer won");
+            let winner = "computer";
+            return roundResult = `You lose, since ${computerSelection} beats ${playerSelection}!`; 
         }  // three possibilities where computer wins
     else if ((computerSelection === "scissors" && playerSelection === "rock") ||
         (computerSelection === "rock" && playerSelection === "paper") ||
         (computerSelection === "paper" && playerSelection === "scissors")) {
-            console.log("working");
-            return winner = "player";
+            console.log("Player won");
+            let winner = "player";
+            return roundResult = `You win, since ${playerSelection} beats ${computerSelection}!`; 
         }  // three possibilities where computer wins  
     else {
-        console.log("working");
-        return "It's a tie!";
+        console.log("Tie");
+        let winner = "tie";
+        return roundResult = `It's a tie, you both chose ${computerSelection}!`;
     }
-}
