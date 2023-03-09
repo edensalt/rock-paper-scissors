099212254b601438d127bbfc269ca41ffd9dda8b
edensalt <124738007+edensalt@users.noreply.github.com>
2023-03-09 11:25:26 -0600

    Show running score total on screen

--- game.js
+++ game.js
@@ -1,4 +1,6 @@
 
+const roundWinners = [];
+
 function playRound(choice) {
 
     // COMPUTER SELECTION
@@ -50,6 +52,68 @@ function playRound(choice) {
     para.textContent = `${winner} won!`;
 
     resultsContainer.appendChild(para);
+
+    //Update score
+
+    roundWinners.push(winner);
+
+    // Count computer wins
+
+    let computerWins = 0;
+    for (let i = 0; i < roundWinners.length; i++) {
+        if (roundWinners[i] === "computer") {
+            ++computerWins
+        }
+    }
+
+    console.log(`The computer has won ${computerWins} times.`);
+
+    // Count player wins
+
+    let playerWins = 0;
+    for (let i = 0; i < roundWinners.length; i++) {
+        if (roundWinners[i] === "player") {
+            ++playerWins
+        }
+    }
+    
+    console.log(`You have won ${playerWins} times.`);
+
+
+    // Count ties  
+
+    let ties = 0;
+    for (let i = 0; i < roundWinners.length; i++) {
+        if (roundWinners[i] === "tie") {
+            ++ties
+        }
+    }
+
+    console.log(`There have been ${ties} ties.`);
+
+    // Set up scoreboard
+
+
+    
+    // Add to scoreboard
+
+    const playerScore = document.querySelector('.player-wins');
+    const paraPlayer = document.createElement('p');
+    paraPlayer.textContent = playerWins;
+
+    playerScore.replaceChild(paraPlayer, playerScore.lastChild);
+
+    const computerScore = document.querySelector('.computer-wins');
+    const paraComputer = document.createElement('p');
+    paraComputer.textContent = computerWins;
+
+    computerScore.replaceChild(paraComputer, computerScore.lastChild);
+
+    const tieScore = document.querySelector('.ties');
+    const paraTie = document.createElement('p');
+    paraTie.textContent = ties;
+
+    tieScore.replaceChild(paraTie, tieScore.lastChild);
 
 }
 
